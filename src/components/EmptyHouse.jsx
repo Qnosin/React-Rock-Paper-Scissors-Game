9a68ecd8be81b8dda52b3e4e631e2d299582d635
Jakub Putaj <64248524+Qnosin@users.noreply.github.com>
2024-02-12 18:43:28 +0100

    Delete src directory

--- src/components/EmptyHouse.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './style/result.css';
-function EmptyHouse() {
-  return (
-    <div className='emptyHouse'></div>
-  )
-}
-
-export default EmptyHouse
--- src/components/hooks/useComputerLogic.js
+++
@@ -1,9 +0,0 @@
-function useComputerLogic() {
-    let tab = ['rock','paper','sicssors'];
-    let randomNum = Math.floor(Math.random() * 3) + 0;
-    console.log(randomNum);
-    let computerPick = tab[randomNum];
-    return {computerPick};
-}
-
-export default useComputerLogic
